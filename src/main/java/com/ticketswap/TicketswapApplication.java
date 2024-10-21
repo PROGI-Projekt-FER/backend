ffc28bcbd1cc178424b45bbd9c7b8beb45e0e74a
matko-papic-fer <matko.papic@fer.unizg.hr>
2024-10-21 21:11:42 +0200

    [B2] [resolves #3] Setup backend project

---
+++ src/main/java/com/ticketswap/TicketswapApplication.java
@@ -0,0 +1,13 @@
+package com.ticketswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketswapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketswapApplication.class, args);
+	}
+
+}
