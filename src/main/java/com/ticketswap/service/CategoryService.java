984cc6741dc44d0e805a0216fb420989ee95b94c
matko-papic-fer <matko.papic@fer.unizg.hr>
2024-11-10 16:18:22 +0100

    [B11] [resolves #26] add crud operations for category

--- src/main/java/com/ticketswap/service/CategoryService.java
+++ src/main/java/com/ticketswap/service/CategoryService.java
@@ -1,7 +1,9 @@
 package com.ticketswap.service;
 
 import com.ticketswap.dto.category.CategoryDto;
+import com.ticketswap.model.Category;
 import com.ticketswap.repository.CategoryRepository;
+import com.ticketswap.util.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -15,5 +17,27 @@ public class CategoryService {
     public List<CategoryDto> getAllCategories() {
         List<CategoryDto> categories = categoryRepository.findAll().stream().map(CategoryDto::map).toList();
         return categories;
+    }
+
+    public CategoryDto createCategory(CategoryDto categoryDto) {
+        Category category = categoryDto.toEntity();
+        category.setId(null);
+        category = categoryRepository.save(category);
+        return CategoryDto.map(category);
+    }
+
+    public CategoryDto updateCategory(CategoryDto categoryDto) {
+        Category category = categoryDto.toEntity();
+        if (category.getId() == null) throw new ResourceNotFoundException("Category Id can not be null");
+        if (categoryRepository.findById(categoryDto.getId()).isEmpty()) throw new ResourceNotFoundException("Category with id " + categoryDto.getId() + " not found");
+        category = categoryRepository.save(category);
+        return CategoryDto.map(category);
+    }
+
+    public void deleteCategory(CategoryDto categoryDto) {
+        Category category = categoryDto.toEntity();
+        if (category.getId() == null) throw new ResourceNotFoundException("Category Id can not be null");
+        if (categoryRepository.findById(categoryDto.getId()).isEmpty()) throw new ResourceNotFoundException("Category with id " + categoryDto.getId() + " not found");
+        categoryRepository.delete(category);
     }
 }
