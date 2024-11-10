d53cdbd353cfbc35b4d616b9bed72ec7910632e7
matko-papic-fer <matko.papic@fer.unizg.hr>
2024-11-11 00:13:18 +0100

    Add redirect to frontend after login

--- src/main/java/com/ticketswap/config/SecurityConfig.java
+++ src/main/java/com/ticketswap/config/SecurityConfig.java
@@ -1,12 +1,23 @@
 package com.ticketswap.config;
 
 import com.ticketswap.service.CustomOAuth2UserService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
@@ -33,6 +44,7 @@ public class SecurityConfig {
                         .userInfoEndpoint(userInfoEndpoint ->
                                 userInfoEndpoint.userService(customOAuth2UserService)
                         )
+                        .successHandler(new CustomOAuth2SuccessHandler())
             )
             .logout( logout ->
                     logout
@@ -42,5 +54,18 @@ public class SecurityConfig {
             );
 
         return http.build();
+    }
+}
+
+@Component
+class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        // Redirect to frontend with the cookie
+        String frontendRedirectUrl = "http://localhost:3000/home"; // Adjust this URL based on your frontend address
+
+        // Optionally, you can add parameters to the redirect URL
+        response.sendRedirect(frontendRedirectUrl);
     }
 }
