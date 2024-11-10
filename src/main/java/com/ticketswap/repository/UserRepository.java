183d3db9d478bf71f7d2f94e2f406fd5ba3eb8a6
matko-papic-fer <matko.papic@fer.unizg.hr>
2024-11-10 12:48:41 +0100

    [B7] [resolves #18] add notification entity and connect with user

--- src/main/java/com/ticketswap/repository/UserRepository.java
+++ src/main/java/com/ticketswap/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.ticketswap.repository;
 
+import com.ticketswap.model.Notification;
 import com.ticketswap.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
