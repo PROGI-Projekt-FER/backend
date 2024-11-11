69d518dcc84444d024ed88e91b7c0a1a0bb02e28
matko-papic-fer <matko.papic@fer.unizg.hr>
2024-11-11 22:32:34 +0100

    [B12] [resolves #28] add interested categories

--- src/main/java/com/ticketswap/controller/CategoryController.java
+++ src/main/java/com/ticketswap/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.ticketswap.controller;
 
 import com.ticketswap.dto.category.CategoryDto;
+import com.ticketswap.dto.category.InsertCategoryDto;
 import com.ticketswap.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,22 +24,21 @@ public class CategoryController {
     }
 
     @PostMapping
-    public ResponseEntity<CategoryDto> createCategory(@RequestBody CategoryDto categoryDto) {
+    public ResponseEntity<CategoryDto> createCategory(@RequestBody InsertCategoryDto categoryDto) {
         CategoryDto createdCategory = categoryService.createCategory(categoryDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdCategory);
     }
 
     @PutMapping("/{categoryId}")
-    public ResponseEntity<CategoryDto> updateCategory(@RequestBody CategoryDto categoryDto, @PathVariable Long categoryId) {
+    public ResponseEntity<CategoryDto> updateCategory(@RequestBody InsertCategoryDto categoryDto, @PathVariable Long categoryId) {
         categoryDto.setId(categoryId);
         CategoryDto updatedCategory = categoryService.updateCategory(categoryDto);
         return ResponseEntity.ok(updatedCategory);
     }
 
     @DeleteMapping("/{categoryId}")
-    public ResponseEntity<Void> deleteCategory(@RequestBody CategoryDto categoryDto, @PathVariable Long categoryId) {
-        categoryDto.setId(categoryId);
-        categoryService.deleteCategory(categoryDto);
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long categoryId) {
+        categoryService.deleteCategory(categoryId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
